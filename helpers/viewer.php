c37df903617e8b7ec1d0f2b2a5a8044fd9806def
Simone Alati <simone.alati@gmail.com>
2022-02-28 15:47:39 +0100

    Bugfix

--- helpers/viewer.php
+++ helpers/viewer.php
@@ -1,7 +1,7 @@
 <?php
 namespace Helpers;
 class Viewer {
-    public static function getView($view, $data) {
+    public static function getView($view, $data = null) {
         require_once __DIR__ . '/../app/views/' . $view . '.tpl.php';
     }
 }
